f4b18c216d4bd1fb01c5edeb03f0ebf6c4e758b3
inkss <me@szyink.com>
2022-09-07 16:50:59 +0800

    去除重复数字

--- packages/artalk/src/editor/plugs/emoticons-plug.ts
+++ packages/artalk/src/editor/plugs/emoticons-plug.ts
@@ -290,7 +290,7 @@ export default class EmoticonsPlug extends EditorPlug {
     this.emoticons.forEach((grp) => {
       if (grp.type !== 'image') return
       Object.entries(grp.items).forEach(([index, item]) => {
-        text = text.split(`:[${item.key}]`).join(`<img src="${item.val}" alt="${item.key}" atk-emoticon="${item.key}">`) // replaceAll(...)
+        text = text.split(`:[${item.key}]`).join(`<img src="${item.val}" alt="${item.key.replace(/\s[0-9]/i,'')}" atk-emoticon="${item.key}">`) // replaceAll(...)
       })
     })
 
